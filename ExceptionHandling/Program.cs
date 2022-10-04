f4a9902610a6caef4c1b7e9ef125ca75a85e9a33
Kennerson Pereira <kennerson.pereira@thoughtworks.com>
2022-10-04 18:27:26 -0300

    create parent exceptions

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -12,10 +12,17 @@ namespace ExceptionHandling
                 var calculator = new Calculator();
                 var result = calculator.Divide(5, 0);
             }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Cant divide by zero");
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine("Arithmetic exception");
+            }
             catch (Exception e)
             {
-                Console.WriteLine(e);
-                throw;
+                Console.WriteLine("Unexpected error");
             }
         }
     }
